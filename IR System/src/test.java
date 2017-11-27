bf0e115f4680835122086935e5533e831fbfe7d9
basti <basti@basti-laptop>
2017-11-27 16:27:57 +0100

    completed indexing for directories

--- IR System/src/test.java
+++ IR System/src/test.java
@@ -1,14 +1,20 @@
+import java.io.Console;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;  //contains Stemming for English
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
@@ -22,24 +28,61 @@ import org.apache.lucene.store.RAMDirectory;
 
 public class test {
 	
-	private IndexWriter writer;
+	private static IndexWriter w;
 	
+	public static Document getDocument(File f) throws Exception {
+		  Document doc = new Document();
+		  Scanner scan = new Scanner(f).useDelimiter("\\A");
+		  Field contents = new TextField("contents", scan.next() , Store.YES); 
+		  Field filename = new TextField("filename", f.getName(), Store.YES);
+		  scan.close();
+		  doc.add(contents);
+		  doc.add(filename);
+		  return doc;
+	}
 	
-	public static void main(String[] args) throws IOException {
-		String directory = "/home/LuceneTest/documents";
-		String index_path = "/home/LuceneTest/indices";
-		String ranking_model = "VS";
-		String query = "";
+	// indexes HTML-files in the directory and its sub-directories
+	public static void searchSubDirectories(File dir) throws Exception {
+		File[] directoryListing = dir.listFiles();
+		if (directoryListing != null)
+		{
+			for (File child : directoryListing) {
+				if(child.isDirectory()) {
+					searchSubDirectories(child);
+				}
+				else
+				{ 
+					if(child.getName().endsWith(".html")) {
+					Document doc = getDocument(child);
+					w.addDocument(doc);
+					}
+				}
+		    }
+		} 
+		else {
+			System.out.println("first argument needs to be a directory");  
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		if(args.length!=4){
+			System.out.println("insufficient Arguments");
+			System.exit(-1);
+		}
+		//save and convert Arguments 
+		File directory = new File(args[0]);
+		File index_file = new File(args[1]);
+		String ranking_model = args[2];
+		String query = args[3];
 		
-		StandardAnalyzer analyzer = new StandardAnalyzer();
-		Directory index = new RAMDirectory();
+		//initiate analyzer and IndexWriter
+		EnglishAnalyzer analyzer = new EnglishAnalyzer();
+		Directory index = FSDirectory.open(index_file.toPath());
 		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-		IndexWriter w = new IndexWriter(index, config);
-		Document doc = new Document();
-		Field titleField = new TextField("Essensliste","Kartoffel, Apfel, Melone",Field.Store.YES);
-		doc.add(titleField);
-        w.addDocument(doc);
-        System.out.println(doc.toString());
+		w = new IndexWriter(index, config);
+		
+		File dir = new File(args[0]);
+		searchSubDirectories(dir);
         w.close();
 	}
 }
